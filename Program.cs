320f67da4fb83c7b8b8201a7a1c923b782c27fa2
Chandan Dey <chandan.dey@wefivesoft.com>
2020-10-05 00:59:44 +0530

    added Hello World programe

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (System.Int32 i = 1; i < 10; i++)
+            {
+                System.Console.WriteLine("Hello World# " + i);
+            }
+        }
+    }
+}
